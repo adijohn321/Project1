9cf29a3530c01d372a2118be6b85c8343eb34ddf
Adil Mabang laut <abdulwahab.018906@cotabato.sti.edu.ph>
2025-04-29 09:32:54 +0800

    date

--- client/src/pages/hris/employee-records.tsx
+++ client/src/pages/hris/employee-records.tsx
@@ -245,10 +245,10 @@ export default function EmployeeRecords() {
                             <Input
                               type="date"
                               {...field}
-                              value={field.value ? new Date(field.value).toISOString().split('T')[0] : ''}
-                              onChange={(e) => {
-                                field.onChange(e.target.value ? new Date(e.target.value).toISOString() : '');
-                              }}
+                              //value={field.value ? new Date(field.value).toISOString().split('T')[0] : ''}
+                              // onChange={(e) => {
+                              //   field.onChange(e.target.value ? new Date(e.target.value).toISOString() : '');
+                              // }}
                             />
                           </FormControl>
                           <FormMessage />
